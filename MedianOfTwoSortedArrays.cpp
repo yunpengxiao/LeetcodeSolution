15f75adde36a731d9d7126abca1a176863c4c277
Yunpeng <yunpeng.shaw@gmail.com>
2014-01-06 16:36:28 -0500

    nothing

--- MedianOfTwoSortedArrays.cpp
+++ MedianOfTwoSortedArrays.cpp
@@ -1,5 +1,14 @@
+//Solution is in http://leetcode.com/2011/03/median-of-two-sorted-arrays.html
+
 #include <iostream>
 using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(int A[], int m, int B[], int n) {
+        
+    }
+};
 
 int main()
 {
