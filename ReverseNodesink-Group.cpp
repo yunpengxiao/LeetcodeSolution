9f001966aed9b5e86f2bb8489b7583f70b4e5882
Yunpeng <yunpeng.shaw@gmail.com>
2014-01-20 22:35:11 -0500

    It's not done by me!

--- ReverseNodesink-Group.cpp
+++ ReverseNodesink-Group.cpp
@@ -7,6 +7,44 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+class Solution 
+{
+private:
+	ListNode *reverse(ListNode *pre, ListNode *nxt)
+	{
+
+        ListNode *last = pre->next;
+        ListNode *cur = last->next;
+        while (cur != nxt)
+        {
+            last->next = cur->next;
+            cur->next = pre->next;
+            pre->next = cur;
+            cur = last->next;
+        }
+        return last;
+	}
+public:
+    ListNode *reverseKGroup(ListNode *head, int k) 
+    {
+        if (head == NULL || k == 1) return head;
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *pre = dummy;
+        int i = 0;
+        while (head != NULL)
+        {
+            i++;
+            if (i % k == 0)
+            {
+                pre = reverse(pre, head->next)
+                head = pre->next;
+            }
+            else head = head->next;
+        } 
+        return dummy->next;
+    }
+};
 
 int main()
 {
