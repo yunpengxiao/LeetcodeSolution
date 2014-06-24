e64de1baadee5e03f9631ba5f4b079099e8ec698
Yunpeng Xiao <yunpeng.shaw@gmail.com>
2014-06-24 07:50:36 -0700

    add java solutions

---
+++ JavaMain.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.io.*;
+
+class Solutions
+{
+	public void reverseWords(String s) 
+	{
+
+    }
+}
+
+public class JavaMain
+{
+	public static void main(String args[])
+	{
+		System.out.println("Yunpeng");
+	}
+}
