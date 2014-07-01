5a20e60f7658c726fd247cd8f37597ba7b03db5a
yunpeng <yunpeng.shaw@gmail.com>
2014-06-30 18:39:11 -0700

    word reverse1

--- JavaMain.java
+++ JavaMain.java
@@ -3,9 +3,31 @@ import java.io.*;
 
 class Solutions
 {
-	public void reverseWords(String s) 
+	public String reverseWords(String s) 
 	{
+		String t = "";
+		int index = 0;
+		for (int i = 0; i < s.length(); i++)
+		{
+			if (i == 0 && s.charAt(i) == ' ') continue;
+			else 
+			{
 
+				if (s.charAt(i) == ' ' && s.charAt(i) == s.charAt(i - 1)) continue;
+				else
+				{
+					t += s.charAt(i);
+				}
+			}
+		}
+		String[] parts = t.split(" ");
+		String result = "";
+		for (int i = parts.length - 1; i >= 0; i--)
+		{
+			result += parts[i];
+			if (i != 0) result += " ";
+		}
+		return result;
     }
 }
 
@@ -14,5 +36,7 @@ public class JavaMain
 	public static void main(String args[])
 	{
 		System.out.println("Yunpeng");
+		Solutions s = new Solutions();
+		System.out.println(s.reverseWords("? nhgpoulnntytmvpqe.rb.d ?s'o?bekcnb?jku?'fc' !ji xsn!ppbuso ei,kwkbhaenntkdqbutwa'a.lizwbtxnejicm t,dab,as.!hftamhwps.!.wkcgqfioxb?qubollgsd.wbjlvhr "));
 	}
 }
