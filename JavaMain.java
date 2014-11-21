2271250bacb31f0aa74b13d84d4f93d89d475da5
Yunpeng Xiao <Kevin@Yunpengs-MBP.home>
2014-11-21 09:59:35 -0500

    Nov 20th 2014

--- JavaMain.java
+++ JavaMain.java
@@ -1,6 +1,8 @@
 import java.lang.*;
 import java.io.*;
 import java.util.Stack;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -22,6 +24,31 @@ class Point
 
 class Solutions
 {
+    public List<Integer> grayCode(int n)
+    {
+        List<Integer> res = new ArrayList<Integer>();
+        if (n < 0)
+            return res;
+        if (n == 0)
+        {
+            res.add(0);
+            return res;
+        }
+
+        res.add(0);
+        res.add(1);
+        for (int i = 2; i <= n; i++)
+        {
+            int size = res.size();
+            for (int j = size - 1; j >= 0; j--)
+            {
+                res.add(res.get(j) + (1 << (i - 1)));
+            }
+        }
+        return res;
+    }
+
+    
     public int maxPoints(Point[] points)
     {
         if (points.length == 0) return 0;
