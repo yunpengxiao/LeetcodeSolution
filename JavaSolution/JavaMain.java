0801c3f5b20ccbc2dd76e7dcf50b178b98d6b3c3
yunpengxiao <yunpeng.shaw@gmail.com>
2015-07-05 10:59:12 -0700

    Fist Java Solution -- Same Tree

---
+++ JavaSolution/JavaMain.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class JavaMain {
+	public static void main(String args[]) {
+		SameTree st = new SameTree();
+		TreeNode p = null, q = null;
+		if (st.isSameTree(p, q))
+			System.out.println("Yes");
+		else
+			System.out.println("No");	
+	}
+}
