d4fcadc55bfb515e1be52fe38e7e433bf0b03951
yunpengxiao <yunpeng.shaw@gmail.com>
2016-02-19 23:29:12 -0800

    sucks

--- JavaSolution/src/Microsoft/IntersectionofTwoLinkedLists.java
+++ JavaSolution/src/Microsoft/IntersectionofTwoLinkedLists.java
@@ -4,5 +4,22 @@ package Microsoft;
  * Created by xyunpeng on 2/11/16.
  */
 public class IntersectionofTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
 
+        ListNode pa = headA;
+        ListNode pb = headB;
+
+        while (true) {
+            if (pa == pb) {
+                return pa;
+            }
+
+            pa = (pa == null)? headB: pa.next;
+            pb = (pb == null)? headA: pb.next;
+        }
+
+    }
 }
