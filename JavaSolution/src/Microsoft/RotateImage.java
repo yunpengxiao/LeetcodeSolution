003f0b00c233cc3868f682baa2a5bbffc313dad6
yunpengxiao <yunpeng.shaw@gmail.com>
2016-02-25 21:50:55 -0800

    bad day

---
+++ JavaSolution/src/Microsoft/RotateImage.java
@@ -0,0 +1,25 @@
+package Microsoft;
+
+/**
+ * Created by xyunpeng on 2/25/16.
+ */
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        int m = matrix.length;
+        for (int i = 0; i < m; i++) {
+            for (int j = i; j < m; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][m - j - 1];
+                matrix[i][m - j - 1] = temp;
+            }
+        }
+    }
+}
---
+++ JavaSolution/src/Microsoft/SpiralMatrix.java
@@ -0,0 +1,50 @@
+package Microsoft;
+
+/**
+ * Created by xyunpeng on 2/25/16.
+ */
+public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+
+        if (matrix == null || matrix.length == 0) {
+            return result;
+        }
+
+        int rowStart = 0;
+        int rowEnd = matrix.length - 1;
+        int colStart = 0;
+        int colEnd = matrix[0].length - 1;
+
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            for (int i = colStart; i <= colEnd; i++) {
+                result.add(matrix[rowStart][i]);
+            }
+            rowStart++;
+
+            for (int i = rowStart; i <= rowEnd; i++) {
+                result.add(matrix[i][colEnd]);
+            }
+            colEnd--;
+
+            if (rowEnd >= rowStart) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    result.add(matrix[rowEnd][i]);
+                }
+            }
+            rowEnd--;
+
+            if (colEnd >= colStart) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    result.add(matrix[i][colStart]);
+                }
+            }
+            colStart++;
+        }
+
+        return result;
+    }
+}
+
+
+//别人家优雅的代码: https://leetcode.com/discuss/12228/super-simple-and-easy-to-understand-solution
