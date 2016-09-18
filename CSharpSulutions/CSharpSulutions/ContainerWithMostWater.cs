2024b955d22216f14155d8449cfd86681c2565c4
Yunpeng Xiao <yunpeng.shaw@gmail.com>
2016-09-17 23:04:20 -0700

    CSharp Solution
    Start a new folder with all the CSharp code

---
+++ CSharpSulutions/CSharpSulutions/ContainerWithMostWater.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSulutions
+{
+    class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            if (height.Length == 0 || height.Length == 1)
+            {
+                return 0;
+            }
+
+            int start = 0;
+            int end = height.Length;
+            int maxArea = 0;
+            while (start < end)
+            {
+                int area = Math.Min(height[start], height[end]) * (end - start);
+                if (maxArea < area)
+                {
+                    maxArea = area;
+                }
+
+                if (height[start] > height[end])
+                {
+                    end++;
+                }
+                else
+                {
+                    start++;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
