627e1cea69ba6307ed29ddaadf2cc70436e51785
Yunpeng Xiao <yunpengxiao@users.noreply.github.com>
2022-07-12 15:04:32 -0700

    Update main.rs

--- rust_solution/src/main.rs
+++ rust_solution/src/main.rs
@@ -1,7 +1,6 @@
 mod two_sum;
 
 fn main() {
-    let _s = two_sum::Solution::new("2 Sum".to_string());
     let nums = [1, 2, 3, 4, 5];
     let result = two_sum::Solution::two_sum(nums.to_vec(), 6);
     println!("{} {}", result[0], result[1]);
