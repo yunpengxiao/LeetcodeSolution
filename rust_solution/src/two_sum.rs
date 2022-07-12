f461352f413286f661772009686cd4fdcbaae973
Yunpeng Xiao <yunpengxiao@users.noreply.github.com>
2022-07-12 15:04:12 -0700

    Update two_sum.rs

--- rust_solution/src/two_sum.rs
+++ rust_solution/src/two_sum.rs
@@ -3,37 +3,28 @@ Given an array of integers nums and an integer target, return the two numbers su
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 */
+use std::collections::HashMap;
 
-pub struct Solution {
-    name: String,
-}
-
-impl Solution {
-    pub fn new(name: String) -> Solution{
-        Solution { name }
-    }
-}
+pub struct Solution {}
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut result = Vec::new();
+    	let mut result = Vec::new();
         if nums.len() < 2 {
             return result;
         }
 
-        let mut v = nums;
-        v.sort();
-        let mut i = 0;
-        let mut j = v.len() - 1;
-        while i < j {
-            if v[i] + v[j] == target {
-                result.push(v[i]);
-                result.push(v[j]);
-                return result;
-            } else if v[i] + v[j] > target {
-                j -= 1;
-            } else {
-                i += 1;
+        let mut hm = HashMap::new();
+        for i in 0..nums.len() {
+            match hm.get(&(target - nums[i])) {
+                Some(&index) => {
+                    result.push(index as i32);
+                    result.push(i as i32);
+                    return result;
+                }
+                _ => {
+                    hm.insert(nums[i], i);
+                }
             }
         }
 
